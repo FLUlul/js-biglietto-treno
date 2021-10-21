b4bcf6baff7efc7b82357260f079094d790229f4
FLU <valerio.massj@gmail.com>
2021-10-21 16:04:15 +0200

    if else code clear :=)

--- js/script.js
+++ js/script.js
@@ -16,44 +16,30 @@ const outputSale = document.getElementById("out-sale");
 
 /* ask user the NUMBER of km he needs to travel */
 const km = parseInt(prompt("Insert the km you have to travel"));
-console.log(km); //console debug
 
 /* ask the AGE of the user */
 const age = parseInt(prompt("Insert your age"));
-console.log(age); //console debug
 
 /* calculate the km insert by user * 0.21 in price*/
-let unitPrice = km * 0.21;
-console.log("full price is: " ,unitPrice); //console debug
+const unitPrice = km * 0.21;
 
 /* if the user is < 18 the result in price is - 20% 
 and if the user is > 65 the result in price is - 40%*/
-
 let percentile;
-let sale;
-
 if (age < 18) {
-    percentile = (20 / 100) * unitPrice;
-    console.log("the percentile to subtract" ,percentile); //console debug
-
-    sale = (unitPrice - percentile).toFixed(2);
-    console.log("the final price under 18 is: ", sale); //console debug
-
+    percentile = 20;
     outputSale.innerHTML = `Your Ticket costs ${unitPrice}€`
-    output.innerHTML = `Your Ticket costs ${sale}€ with 20% sale`
 } else if (age > 65) {
-    percentile = (40 / 100) * unitPrice;
-    console.log("the percentile to subtract" ,percentile); //console debug
-
-    sale = (unitPrice - percentile).toFixed(2);
-    console.log("the final price over 65 is: ", sale); //console debug
-
+    percentile = 40;
     outputSale.innerHTML = `Your Ticket costs ${unitPrice}€`
-    output.innerHTML = `Your Ticket costs ${sale}€ with 40% sale`
 } else {
-    output.innerHTML = `Your Ticket costs ${unitPrice}€`
-    console.log("the final price is: ",unitPrice) //console debug
+    percentile = 0;
 }
 
+const discount = (percentile / 100) * unitPrice;
+const sale = (unitPrice - discount).toFixed(2);
+output.innerHTML = `Your Ticket costs ${unitPrice}€`
+output.innerHTML = `Your Ticket costs ${sale}€`
 
-/* output the final price with max 2 decimals */
+
+
